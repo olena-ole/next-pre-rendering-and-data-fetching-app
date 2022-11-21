2b2bec8635ce5a1faf7c9c57991c524d5ad9829d
Olena <pampam32121@gmail.com>
2022-11-22 01:40:06 +0200

    added user profile page, rendered on the server side

---
+++ pages/user-profile.js
@@ -0,0 +1,19 @@
+export default function UserProfilePage(props) {
+    return (
+        <h1>{props.username}</h1>
+    )
+}
+
+export async function getServerSideProps(context) {
+
+    // const { params, req, res } = context
+
+    // console.log(req)
+    // console.log(res)
+
+    return {
+        props: {
+            username: 'Max'
+        }
+    }
+}
