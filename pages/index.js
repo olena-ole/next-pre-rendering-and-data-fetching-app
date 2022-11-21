1f933dc0ebb4a2d22bc6ae02a0f6041712d889b1
Olena <pampam32121@gmail.com>
2022-11-21 15:31:41 +0200

    received dummy-data processing the files in project file system

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,6 @@
+import fs from 'fs/promises'
+import path from 'path'
+
 function HomePage(props) {
 
   const { products } = props
@@ -11,9 +14,13 @@ function HomePage(props) {
 
 export async function getStaticProps() {
 
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json')
+  const jsonData = await fs.readFile(filePath)
+  const data = JSON.parse(jsonData)
+
   return {
     props: {
-      products: [{id: 'p1', title: 'Product1'}]
+      products: data.products
     }
   }
 }
