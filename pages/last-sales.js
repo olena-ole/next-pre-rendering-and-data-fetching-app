2b1e6cbdf366ce6bdee261b0cfd2c44e1e6aa7a8
Olena <pampam32121@gmail.com>
2022-11-22 18:32:31 +0200

    implemented the same data fetching with useSWR hook and defaul fetcher function

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -3,54 +3,39 @@ import useSWR from 'swr'
 
 export default function lastSalesPage() {
 
-    // const [sales, setSales] = useState()
-
-    const fetcher = (url) => fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            const transformedSales = []
-
-            for (let key in data) {
-                transformedSales.push({
-                    id: key,
-                    ...data[key]
-                })
-            }
-
-            return transformedSales
-        })
+    const [sales, setSales] = useState()
 
     const { data, error } = useSWR(
-        'https://next-course-data-fetchin-aa929-default-rtdb.firebaseio.com/sales.json', 
-        fetcher 
+        'https://next-course-data-fetchin-aa929-default-rtdb.firebaseio.com/sales.json',
+        (url) => fetch(url).then(res => res.json())
     )
 
-    // useEffect( () => {
-    //     const transformedSales = []
+    useEffect( () => {
+        const transformedSales = []
 
-    //     for (let key in data) {
-    //         transformedSales.push({
-    //             id: key,
-    //             ...data[key]
-    //         })
-    //     }
+        for (let key in data) {
+            transformedSales.push({
+                id: key,
+                ...data[key]
+            })
+        }
 
-    //     setSales(transformedSales)
-    // }, [data])
+        setSales(transformedSales)
+    }, [data])
 
 
     if (error) {
         return <p>Failed to load!</p>
     }
 
-    if (!data) {
+    if (!data || !sales) {
         return <p>Loading...</p>
     }
 
 
     return (
         <ul>
-            {data.map(sale => (
+            {sales.map(sale => (
                 <li key={sale.id}>
                     {sale.username} - ${sale.volume}
                 </li>
